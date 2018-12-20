50ffcc7e3dfe8599971742df01ad44eae4145514
lucieauge <lucie.auge@gmail.com>
2018-12-20 19:42:42 +0100

    changement de l'image firefox dans main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,11 @@
-let monTitre = document.querySelector('h1');
-monTitre.textContent = 'Hello world!';
+let monImage = document.querySelector('img');
 
+monImage.addEventListener('click', function() {
+    let monSrc = monImage.getAttribute('src');
+    if (monSrc === 'images/firefox-icon.png') {
+      monImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+      monImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+});
 
