7a0932e88db167431bb012ccd2f27a8e0424bdea
lucieauge <lucie.auge@gmail.com>
2018-12-20 20:54:42 +0100

    Ajout bouton changer d'utilisateur + pop up qui demande le nom d'utilisateur

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,22 @@ monImage.addEventListener('click', function() {
     }
 });
 
+let monBouton = document.querySelector('button');
+let monTitre = document.querySelector('h1');
+
+function définirNomUtilisateur() {
+  let monNom = prompt('Veuillez saisir votre nom.');
+  localStorage.setItem('nom', monNom);
+  monTitre.textContent = 'Mozilla est cool, ' + monNom;
+}
+
+if (!localStorage.getItem('nom')) {
+  définirNomUtilisateur();
+} else {
+  let nomEnregistré = localStorage.getItem('nom');
+  monTitre.textContent = 'Mozilla est cool, ' + nomEnregistré;
+}
+
+monBouton.addEventListener('click', function() {
+  définirNomUtilisateur();
+});
